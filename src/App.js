7b66a31a4bde12bd2fab38511232733af0b78d83
kusumasaisudha <kusumasaisudhabuddha@gmail.com>
2023-11-24 13:42:50 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import Login from './components/Login'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+
+import JobItemDetails from './components/JobItemDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" Component={Login} />
+        <ProtectedRoute exact path="/" Component={Home} />
+        <ProtectedRoute exact path="/jobs" Component={Jobs} />
+        <ProtectedRoute exact path="/jobs/:id" Component={JobItemDetails} />
+        <Route path="/not-found" Component={NotFound} />
+        <Redirect to="not-found" />
+      </Switch>
+    )
+  }
+}
+
+export default App
